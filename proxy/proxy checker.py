62e621ad878c1190352605345cc160eaaee2b889
Nirgranthi <223480893+nirgranthi@users.noreply.github.com>
2026-01-06 12:23:01 +0530

    updated proxy checker

--- proxy/proxy checker.py
+++ proxy/proxy checker.py
@@ -2,9 +2,14 @@ import requests
 import concurrent.futures
 import threading
 import urllib3
+import itertools
 
-http_proxies = 'https://raw.githubusercontent.com/wiki/gfpcom/free-proxy-list/lists/http.txt'
-https_proxies = 'https://raw.githubusercontent.com/wiki/gfpcom/free-proxy-list/lists/https.txt'
+# 400000+ proxies for each
+#http_proxies_raw = 'https://raw.githubusercontent.com/wiki/gfpcom/free-proxy-list/lists/http.txt'
+#https_proxies_raw = 'https://raw.githubusercontent.com/wiki/gfpcom/free-proxy-list/lists/https.txt'
+
+http_proxies_raw = 'https://raw.githubusercontent.com/TheSpeedX/PROXY-List/refs/heads/master/http.txt'  #40000+ proxies
+https_proxies_raw = ''
 
 GREEN = '\033[92m'
 RED = '\033[91m'
@@ -13,33 +18,37 @@ RESET = '\033[0m'
 
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
-def load_proxies():
+lock = threading.Lock()
+counter = itertools.count(1)
+
+def loadProxies(url, proxyType):
     proxies = []
     try:
-        print(f"{YELLOW}Fetching proxies...{RESET}")
-        response = requests.get(http_proxies)
+        print(f"{YELLOW}Fetching {proxyType} proxies...{RESET}")
+        response = requests.get(url)
         proxies.extend([line.strip() for line in response.text.splitlines() if line.strip()])
     except Exception as e:
-        print(f"{YELLOW}[!] Failed to load HTTP proxies: {e}{RESET}")
-    try:
-        response = requests.get(https_proxies)
-        proxies.extend([line.strip() for line in response.text.splitlines() if line.strip()])
-    except Exception as e:
-        print(f"{YELLOW}[!] Failed to load HTTPS proxies: {e}{RESET}")
-    #print(f"{GREEN}Proxies loaded successfully.{RESET}")
-    print(f"{GREEN}Total proxies fetched: {len(proxies)}{RESET}")
+        print(f"{YELLOW}[!] Failed to load {proxyType} proxies: {e}{RESET}")
     return list(set(proxies))
 
-def check_proxy(proxy, counter, working_proxies, lock):
-    with lock:
-        count = next(counter)
+#print(f"{GREEN}Total proxies fetched: {len(http_proxies)+len(http)}{RESET}")
+
+def check_proxy(proxy, protocol, fileHandle):
+    count = next(counter)
+    targetUrl = 'https://httpbin.org/ip' if protocol=='https' else 'http://httpbin.org/ip'
     try:
         #verify argument checks for SSL certificates
-        response = requests.get('http://httpbin.org/ip', proxies={'http': proxy, 'https': proxy}, timeout=5, verify=False)
+        response = requests.get(
+            targetUrl, 
+            proxies={'http': proxy, 'https': proxy}, 
+            timeout=5, 
+            verify=False
+        )
         if response.status_code == 200:
-            print(f"{GREEN}{count}:- Working proxy: {proxy}{RESET}")
             with lock:
-                working_proxies.append(proxy)
+                print(f"{GREEN}{count}:- Working {protocol}: {proxy}{RESET}")
+                fileHandle.write(f'{proxy}\n')
+                fileHandle.flush()
         else:
             pass
             #print(f"{RED}[!]{count}:- Failed to check proxy {proxy} (status: {response.status_code}){RESET}")
@@ -48,18 +57,23 @@ def check_proxy(proxy, counter, working_proxies, lock):
         #print(f"{RED}[!]{count}:- Failed to check proxy {proxy}{RESET}")
         #print(f"{RED}[!]{count}:- Failed to check proxy {proxy}: {e}{RESET}")
 
-proxies = load_proxies()
-working_proxies = []
-counter = iter(range(1, len(proxies) + 1))
-lock = threading.Lock()
+def main():
+    http_proxies = loadProxies(http_proxies_raw, 'HTTP')
+    https_proxies = loadProxies(http_proxies_raw, 'HTTPS')
 
-with concurrent.futures.ThreadPoolExecutor(max_workers=200) as executor:
-    futures = [executor.submit(check_proxy, proxy, counter, working_proxies, lock) for proxy in proxies]
-    concurrent.futures.wait(futures)
+    with open('working_http_proxies.txt', 'w') as httpFile, \
+        open('working_https_proxies.txt', 'w') as httpsFile, \
+        concurrent.futures.ThreadPoolExecutor(max_workers=200) as executor:
 
-with open('working_proxies.txt', 'w') as f:
-    for proxy in working_proxies:
-        f.write(f'{proxy}\n')
+        futures = []
+        for proxy in http_proxies:
+            futures.append(executor.submit(check_proxy, proxy, 'http', httpFile))
+        
+        for proxy in https_proxies:
+            futures.append(executor.submit(check_proxy, proxy, 'https', httpsFile))
 
-print(f"{GREEN}Total working proxies: {len(working_proxies)}{RESET}")
+        concurrent.futures.wait(futures)
 
+#print(f"{GREEN}Total working proxies: {len(working_http_proxies)+len(working_https_proxies)}{RESET}")
+if __name__ == '__main__':
+    main()
