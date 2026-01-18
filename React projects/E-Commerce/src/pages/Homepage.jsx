37c2289f8a6904eb81b3155647b5131074523bda
Nirgranthi <223480893+nirgranthi@users.noreply.github.com>
2026-01-18 15:07:48 +0530

    add server proxy for /api and /images in vite config

--- React projects/E-Commerce/src/pages/Homepage.jsx
+++ React projects/E-Commerce/src/pages/Homepage.jsx
@@ -8,11 +8,11 @@ export default function Homepage() {
     const [cart, setCart] = useState([])
 
     useEffect(() => {
-        axios.get('http://localhost:3000/api/products')
+        axios.get('/api/products')
             .then((response) => {
                 setProducts(response.data)
             })
-        axios.get('http://localhost:3000/api/cart-items')
+        axios.get('/api/cart-items')
             .then((cartItems) => {
                 setCart(cartItems.data)
             })
