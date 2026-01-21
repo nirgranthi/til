81e577f92706d46bf3e9300d0fcb1794504fad7f
Nirgranthi <223480893+nirgranthi@users.noreply.github.com>
2026-01-21 10:49:37 +0530

    add 'fileline reader for small file.py'

---
+++ efficient fileline readers/fileline reader for small file.py
@@ -0,0 +1,11 @@
+def count_lines(filename):
+    with open(filename, "rb") as f:
+        megaBytes = 1024*1024 # (this is 1MB)if needed to increase MBs, just multiply "*number"
+
+        #this has error rate of 1 line because it counts new lines
+        return sum(chunk.count(b'\n') for chunk in iter(lambda: f.read(megaBytes), b'')) 
+        
+
+filename = 'passwords.txt'
+print(count_lines(filename))
+
